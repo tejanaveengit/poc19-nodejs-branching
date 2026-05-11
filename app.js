60a9ff6bd76938e7ea2c95ae3a4482918a2f4f40
tejanaveengit <peesatejanaveen@gmail.com>
2026-05-11 16:09:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
 
   if (req.url === "/") {
-    res.end("Welcome to CI/CD Demo 🚀");
+    res.end("Welcome to CI/CD NodeJS Demo 🚀");
   } else if (req.url === "/health") {
     res.end("Application is healthy ✅");
   } else if (req.url === "/about") {
