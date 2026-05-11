4030059b75f4b79d89f6f65d270addcfc6fb3715
tejanaveengit <peesatejanaveen@gmail.com>
2026-05-11 14:39:11 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+
+  if (req.url === "/") {
+    res.end("Welcome to CI/CD Demo 🚀");
+  } else if (req.url === "/health") {
+    res.end("Application is healthy ✅");
+  } else if (req.url === "/about") {
+    res.end("This is a simple Node.js CI/CD app.");
+  } else {
+    res.statusCode = 404;
+    res.end("Page not found ❌");
+  }
+});
